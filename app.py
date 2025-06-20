8971ac498cbaa68cf98ecf099f0c547c623acda2
Mlink <indiegomail@gmail.com>
2025-06-20 19:23:33 +0300

    card correction

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template_string, request, redirect, url_for
 from models import db, Player
 import os
 
